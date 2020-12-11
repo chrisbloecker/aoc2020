9bfcdd0ae812aedd933259436614e12f4c67a93a
Christopher Blöcker <mail@chrisbloecker.se>
2020-12-11 14:45:15 +0100

    input and solution for day 11

---
+++ 11/day11.hs
@@ -0,0 +1,133 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-16.24 script
+
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Main
+  where
+--------------------------------------------------------------------------------
+import Data.Text                    (Text)
+import Data.Void                    (Void)
+import Data.Vector                  (Vector, (!), fromList)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+--------------------------------------------------------------------------------
+import qualified Data.Text.IO as T  (readFile)
+import qualified Data.Vector  as V  (take, drop, foldr, null, filter, length)
+--------------------------------------------------------------------------------
+
+data Matrix a = Matrix { height :: Int
+                       , width  :: Int
+                       , matrix :: Vector a
+                       }
+  deriving (Eq)
+
+instance Show a => Show (Matrix a) where
+  show m@Matrix{..} =
+    if null matrix
+      then ""
+      else V.foldr (\e s -> show e ++ s) "\n" (V.take width matrix) ++
+           show m { matrix = V.drop width matrix }
+
+at :: Matrix a -> Seat -> a
+at Matrix{..} (Seat x y) = matrix ! ((y `mod` height) * width + (x `mod` width))
+
+data Cell = Free
+          | Taken
+          | Floor
+  deriving (Eq)
+
+instance Show Cell where
+  show Free  = "L"
+  show Taken = "#"
+  show Floor = "."
+
+data Seat = Seat Int Int
+
+data Direction = N | NE | E | SE | S | SW | W | NW deriving (Enum, Bounded)
+
+(+>) :: Seat -> Direction -> Seat
+(+>) (Seat x y) = \case N  -> Seat  x    (y-1)
+                        NE -> Seat (x+1) (y-1)
+                        E  -> Seat (x+1)  y
+                        SE -> Seat (x+1) (y+1)
+                        S  -> Seat  x    (y+1)
+                        SW -> Seat (x-1) (y+1)
+                        W  -> Seat (x-1)  y
+                        NW -> Seat (x-1) (y-1)
+
+inBounds :: Matrix a -> Seat -> Bool
+inBounds Matrix{..} (Seat x y) = 0 <= x && x < width
+                              && 0 <= y && y < height
+
+type DecisionRule = Int -> Bool
+type Neighbours   = Matrix Cell -> Seat -> [Seat]
+
+--------------------------------------------------------------------------------
+
+type Parser = Parsec Void Text
+
+parseInput :: Parser (Matrix Cell)
+parseInput = do
+  rows <- sepBy (many cell) eol <* eof
+
+  let height = length . filter (not . null) $ rows
+      width  = length . head $ rows
+      matrix = fromList . concat $ rows
+
+  return Matrix{..}
+
+    where
+      cell :: Parser Cell
+      cell = choice [ char 'L' >> return Free
+                    , char '#' >> return Taken
+                    , char '.' >> return Floor
+                    ]
+
+--------------------------------------------------------------------------------
+
+neighbours :: Neighbours
+neighbours m s = [ s +> d | d <- [minBound .. maxBound], inBounds m (s +> d)]
+
+lineOfSight :: Neighbours
+lineOfSight m s = [ s >+ d | d <- [minBound .. maxBound], inBounds m (s >+ d)]
+  where
+    (>+) :: Seat -> Direction -> Seat
+    s >+ d = let s' = s +> d
+             in if not (inBounds m s') || at m s' /= Floor
+                  then s'
+                  else s' >+ d
+
+takeOrLeave :: Neighbours -> DecisionRule -> DecisionRule -> Matrix Cell -> Seat -> Cell
+takeOrLeave neighbours takeIf leaveIf m@Matrix{..} s = case m `at` s of
+    Free  -> if takeIf  takenNeighbours then Taken else Free
+    Taken -> if leaveIf takenNeighbours then Free  else Taken
+    Floor -> Floor
+  where
+    takenNeighbours :: Int
+    takenNeighbours = length . filter (== Taken) . map (at m) $ neighbours m s
+
+fixpoint :: Neighbours -> DecisionRule -> DecisionRule -> Matrix Cell -> Matrix Cell
+fixpoint neighbours takeIf leaveIf m@Matrix{..} =
+  let next = m { matrix = fromList . map (takeOrLeave neighbours takeIf leaveIf m) $ seats }
+  in if m == next
+       then m
+       else fixpoint neighbours takeIf leaveIf next
+
+  where
+      seats :: [Seat]
+      seats = [Seat x y | y <- [0 .. height-1], x <- [0 .. width-1]]
+
+main :: IO ()
+main = do
+  input <- parse parseInput "input.txt" <$> T.readFile "input.txt"
+
+  case input of
+    Left bundle -> putStr (errorBundlePretty bundle)
+    Right m -> do
+      let fp1 = fixpoint neighbours  (== 0) (>= 4) m
+      print $ "(1) " ++ show (V.length . V.filter (== Taken) . matrix $ fp1)
+
+      let fp2 = fixpoint lineOfSight (== 0) (>= 5) m
+      print $ "(2) " ++ show (V.length . V.filter (== Taken) . matrix $ fp2)
